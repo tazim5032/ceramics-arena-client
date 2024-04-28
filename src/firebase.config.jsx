c5bab143bd7714e1c313f85dfb329a7a5be3a3f8
tazim5032 <51076835+tazim5032@users.noreply.github.com>
2024-04-29 02:05:40 +0600

    env variable added

--- src/firebase.config.jsx
+++ src/firebase.config.jsx
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDcp98u4Lo-M7febEEc0bIzb1NoAx1m-iI",
-  authDomain: "ceramics-arena-client.firebaseapp.com",
-  projectId: "ceramics-arena-client",
-  storageBucket: "ceramics-arena-client.appspot.com",
-  messagingSenderId: "867229452576",
-  appId: "1:867229452576:web:0673960dd359b1b6bbef45"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
